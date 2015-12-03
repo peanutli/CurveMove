4f78f0be5c16c493c5b01001e5020fa0705ee8a1
bjlidachuan <bjlidachuan@163.com>
2015-12-03 12:06:35 +0800

    添加了部分测试代码

--- CurveMoveDemo/CurveMoveDemo/ViewController.m
+++ CurveMoveDemo/CurveMoveDemo/ViewController.m
@@ -12,10 +12,37 @@
 
 @end
 
+static NSString * const kCurveMoveKey = @"cureMove";
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIView * point = [[UIView alloc] initWithFrame:CGRectMake(5, 5, 5, 5)];
+    [self.view addSubview:point];
+    point.backgroundColor = [UIColor orangeColor];
+    
+    UIView * circle = [[UIView alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
+    [self.view addSubview:circle];
+    circle.backgroundColor = [UIColor greenColor];
+    circle.alpha = 0.5;
+    
+    //用CGMutablePathRef来创建路线
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddEllipseInRect(path, NULL, circle.frame);
+    
+    CAKeyframeAnimation *animate = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    animate.delegate =self;
+    animate.duration = 3;
+    animate.fillMode = kCAFillModeForwards;
+    animate.repeatCount = 50;
+    animate.path = path;
+    animate.removedOnCompletion = YES;
+    CGPathRelease(path);
+    [point.layer addAnimation:animate forKey:kCurveMoveKey];
+
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
